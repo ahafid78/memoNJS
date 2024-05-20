92237c4b9a0c78a6463151dae8417e3bf83f2fdd
abdelhafid <ahafid78@gmail.com>
2024-05-20 20:49:49 +0100

    first commit for memoNJS

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+
+const sequelize = require("./src/db/sequelize");
+
+const app = express();
+const port = 3000;
+
+// Ne pas oublier l'installation des middlewares (dans le terminal avec la commande : npm install nom-middleware --save-dev) et l'importation avec : (const nom-middleware = require('nom-depende-middleware'))
+app
+  .use(favicon(__dirname + "/favicon.ico"))
+  .use(morgan("dev"))
+  .use(bodyParser.json());
+
+sequelize.initDb();
+
+// Ici, nous placerons nos futurs points de terminison
+
+require("./src/routes/findAllPokemons")(app);
+require("./src/routes/findAllPokemonByPK")(app);
+require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
+
+app.listen(port, () =>
+  console.log(
+    `Notre application Node est démarrée sur : http://localhost:${port}`
+  )
+);
